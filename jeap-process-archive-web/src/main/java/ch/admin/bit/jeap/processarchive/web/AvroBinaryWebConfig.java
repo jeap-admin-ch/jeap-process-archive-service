700f1314b6cb1b010498ba408d9fdc3e70656662
Fabien Cerf <fabien.cerf@bit.admin.ch>
2025-11-07 15:15:45 +0100

    JEAP-6062: Breaking Change: remove webflux support

--- jeap-process-archive-web/src/main/java/ch/admin/bit/jeap/processarchive/web/AvroBinaryWebConfig.java
+++ jeap-process-archive-web/src/main/java/ch/admin/bit/jeap/processarchive/web/AvroBinaryWebConfig.java
@@ -1,8 +1,6 @@
 package ch.admin.bit.jeap.processarchive.web;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -10,16 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 public class AvroBinaryWebConfig {
 
-    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
     @Bean
     public AvroBinaryHttpMessageConverter avroBinaryHttpMessageConverter() {
         return new AvroBinaryHttpMessageConverter();
     }
 
-    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
-    @Bean
-    public CodecCustomizer avroCodeCustomizer() {
-        return (configurer) ->
-                configurer.customCodecs().register(new AvroBinaryEncoder());
-    }
 }
